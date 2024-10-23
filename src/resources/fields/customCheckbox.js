3a057e92501758f1516b58f77dfb2cadce98057d
lego7set <84934297+lego7set@users.noreply.github.com>
2024-10-22 22:37:29 -0400

    Create customCheckbox.js

---
+++ src/resources/fields/customCheckbox.js
@@ -0,0 +1,23 @@
+export default function registerField() {
+  Blockly.FieldBetterCheckbox = function(opt_value) {
+      Blockly.FieldCheckbox.call(this, opt_value);
+  };
+  Blockly.utils.object.inherits(Blockly.FieldBetterCheckbox, Blockly.FieldCheckbox);
+  
+  Blockly.FieldBetterCheckbox.prototype.init = function() {
+      Blockly.FieldBetterCheckbox.superClass_.init.call(this);
+      this.setValue(this.getValue());
+  };
+  
+  Blockly.FieldBetterCheckbox.prototype.getText = function() {
+      return this.getValue() ? '✓' : '✗';
+  };
+  
+  
+  Blockly.FieldBetterCheckbox.prototype.render = function() {
+      this.textElement_.textContent = this.getText();
+      Blockly.FieldBetterCheckbox.superClass_.render.call(this);
+  };
+  
+  Blockly.fieldRegistry.register('field_better_checkbox', Blockly.FieldBetterCheckbox);
+}
