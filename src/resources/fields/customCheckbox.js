426e584611d73e7cbf394c524c7ad3a8998c053d
lego7set <84934297+lego7set@users.noreply.github.com>
2024-10-22 23:34:14 -0400

    idk whats wrong

--- src/resources/fields/customCheckbox.js
+++ src/resources/fields/customCheckbox.js
@@ -3,6 +3,11 @@ import Blockly from 'blockly/core';
 export default function registerField() {
   console.log(Object.keys(Blockly));
   console.log(Object.keys(Blockly.FieldCheckbox.prototype));
+  
+  const fromJson = function(options) {
+    this.setValue(!!options['checked']);
+  };
+  
   Blockly.FieldBetterCheckbox = function(opt_value) {
       Blockly.FieldCheckbox.call(this, opt_value);
   };
@@ -13,6 +18,7 @@ export default function registerField() {
   Blockly.FieldBetterCheckbox.prototype.init = function() {
       Blockly.FieldBetterCheckbox.superClass_.init.call(this);
       this.setValue(this.getValue());
+      this.fromJson = fromJson.bind(this);
   };
   
   Blockly.FieldBetterCheckbox.prototype.getText = function() {
@@ -23,10 +29,6 @@ export default function registerField() {
   Blockly.FieldBetterCheckbox.prototype.render = function() {
       this.textElement_.textContent = this.getText();
       Blockly.FieldBetterCheckbox.superClass_.render.call(this);
-  };
-
-  Blockly.FieldBetterCheckbox.prototype.fromJson = function(options) {
-    this.setValue(!!options['checked']);
   };
   
   Blockly.fieldRegistry.register('field_better_checkbox', Blockly.FieldBetterCheckbox);
