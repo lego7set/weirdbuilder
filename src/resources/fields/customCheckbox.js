f14ae9c671e8fa4aff50918421c54d2e28b16b5d
lego7set <84934297+lego7set@users.noreply.github.com>
2024-10-23 00:20:31 -0400

    pls work

--- src/resources/fields/customCheckbox.js
+++ src/resources/fields/customCheckbox.js
@@ -5,34 +5,21 @@ export default function registerField() {
   console.log(Object.keys(Blockly.FieldCheckbox.prototype));
 
   Blockly.fieldRegistry.unregister('field_better_checkbox');
-  
-  Blockly.FieldBetterCheckbox = function(opt_value) {
-      Blockly.FieldCheckbox.call(this, opt_value);
-  };
-  Blockly.FieldBetterCheckbox.prototype = Object.create(Blockly.FieldCheckbox.prototype);
-  Blockly.FieldBetterCheckbox.prototype.constructor = Blockly.FieldBetterCheckbox;
-  Blockly.FieldBetterCheckbox.superClass_ = Blockly.FieldCheckbox.prototype;
-  
-  Blockly.FieldBetterCheckbox.prototype.init = function() {
-      Blockly.FieldBetterCheckbox.superClass_.init.call(this);
-      this.setValue(this.getValue());
-  };
-  
-  Blockly.FieldBetterCheckbox.prototype.getText = function() {
-      return this.getValue() ? '✓' : '✗';
-  };
-  
-  
-  Blockly.FieldBetterCheckbox.prototype.render_ = function() {
-      console.log(this.getTextElement());
-      Blockly.FieldBetterCheckbox.superClass_.render_.call(this);
-  };
 
-  Blockly.FieldBetterCheckbox.fromJson = function(options) {
-    const checked = Boolean(Blockly.utils.parsing.replaceMessageReferences(
-      options['checked']));
-    return new (Blockly.FieldBetterCheckbox)(checked);
-  }
+  Blockly.FieldBetterCheckbox = (
+    class btcheckbox extends Blockly.FieldCheckbox {
+      constructor(value) {
+        super(value);
+      }
+      initView() {
+        super.initView();
+        textElement.style.display = "block"; // override none
+      }
+      getDisplayText_() {
+        return this.getValueBoolean() ? '✓' : '✗';
+      }
+    }
+  )
   
   Blockly.fieldRegistry.register('field_better_checkbox', Blockly.FieldBetterCheckbox);
 }
